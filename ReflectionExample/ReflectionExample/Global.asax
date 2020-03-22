ad292061d1e236323688422f3c867b8dd905ff9b
Anand Uperpelli <anand_uperpelli@epam.com>
2020-03-22 19:00:39 +0530

    Reflection

---
+++ ReflectionExample/ReflectionExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReflectionExample.MvcApplication" Language="C#" %>
---
+++ ReflectionExample/ReflectionExample/Models/ControllerAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReflectionExample.Models
+{
+    public class ControllerAction
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string ReturnType { get; set; }
+}
+}
